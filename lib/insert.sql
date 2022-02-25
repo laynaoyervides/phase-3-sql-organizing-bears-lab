43f3b519878184b9530e5a8d331a98f5cca0f03e
Layna Oyervides <laynaoyervides@gmail.com>
2022-02-24 21:39:21 -0700

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 6, "M", "brown", "chill", 0 );
+INSERT INTO bears VALUES (2, "Rowdy", 5, "M", "brown", "chill", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 5, "F", "brown", "chill", 0);
+INSERT INTO bears VALUES (4, "Seargeant Brown", 5, "M", "brown", "chill", 0);
+INSERT INTO bears VALUES (5, "Melissa", 5, "F", "brown", "chill", 0);
+INSERT INTO bears VALUES (6, "Grinch", 5, "M", "brown", "chill", 0);
+INSERT INTO bears VALUES (7, "Wendy", 5, "F", "brown", "chill", 0);
+INSERT INTO bears VALUES (8, null, 4, "M", "brown", "chill", 1);
